4fc24aeb9c84081556e654259fd7b639638b824f
김연수 <chi8850@naver.com>
2023-02-15 12:18:02 +0900

    first commit

---
+++ test.c
@@ -0,0 +1,36 @@
+// #include <stdio.h>
+
+// int main(void)
+// {
+//     int arr[5];     // 길이가 5인 정수형 arr 배열 초기화
+//     int sum = 0, i;
+
+//     arr[0] = 10, arr[1] = 20, arr[2] = 30, arr[3] = 40, arr[4] = 50;
+
+//     for(i=0; i<5; i++)
+//         sum += arr[i];
+
+//     printf("배열요소에 저장된 값의 합: %d \n", sum);
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+// void ShowArayElem(int * param, int len)
+// {
+//     int i;
+//     for(i=0; i<len; i++)
+//         printf("%d ", param[i]);
+//     printf("\n");
+// }
+
+// int main(void)
+// {
+//     int arr1[3] = {1, 2, 3};
+//     int arr2[5] = {4, 5, 6, 7, 8};
+//     printf("%d %d %d \n", sizeof(arr1), sizeof(int), sizeof(arr1)/sizeof(int));
+//     // ShowArayElem(arr1, sizeof(arr1)/sizeof(int));
+//     // ShowArayElem(arr2, sizeof(arr2)/sizeof(int));
+
+//     return 0;
+// }
